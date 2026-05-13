c8746f38eb313942e1851e41c9f08db7c940a4df
Divasdev <sharmadivas881@gmail.com>
2026-05-13 20:36:47 +0530

    made the statscard ans statgrid

---
+++ Weather-App/src/components/StatsGrid/StatsGrid.jsx
@@ -0,0 +1,14 @@
+function StatsCard() {
+  return (
+    <div className="stat-card">
+      <div className="stat-top">
+        <div className="stat-icon">🌪️</div>
+        <span className="stat-title">Air Quality</span>
+      </div>
+
+      <h3 className="stat-value">156</h3>
+    </div>
+  );
+}
+
+export default StatsCard;
