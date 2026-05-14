ac12d05c63153806cb7aabbee694ec8eae3b1be3
Divasdev <sharmadivas881@gmail.com>
2026-05-14 16:18:15 +0530

    UI COMPLETED

--- Weather-App/src/components/Forecast/ForecastList.jsx
+++ Weather-App/src/components/Forecast/ForecastList.jsx
@@ -16,6 +16,7 @@ export function ForecastList(){
           <ForecastRow />
           <ForecastRow />
           <ForecastRow />
+          <ForecastRow />
         </div>
 
         <div className="scroll-indicator">▼</div>
