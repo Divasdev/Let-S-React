b37d62f3400abb46e3bb7f0c694148aafcf3837a
Divasdev <sharmadivas881@gmail.com>
2026-05-14 11:29:25 +0530

    main layout

---
+++ Weather-App/src/components/Layout/MainLayout.jsx
@@ -0,0 +1,21 @@
+
+
+import "./MainLayout.css";
+
+export function MainLayout() {
+  return (
+    <div className="main-layout">
+
+      {/* Left Column */}
+      <div className="left-panel">
+        <h2>Left Panel</h2>
+      </div>
+
+      {/* Right Column */}
+      <div className="right-panel">
+        <h2>Right Panel</h2>
+      </div>
+
+    </div>
+  );
+}
