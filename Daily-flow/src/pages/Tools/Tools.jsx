6e65563d59d14c10fdb2d960de107c2506e938bf
Divasdev <sharmadivas881@gmail.com>
2026-06-05 14:12:30 +0530

    Update Tools page component

--- Daily-flow/src/pages/Tools/Tools.jsx
+++ Daily-flow/src/pages/Tools/Tools.jsx
@@ -1,15 +1,17 @@
 import { Navbar } from "../../components/layout/Navbar";
 import { Footer } from "../../components/layout/Footer";
 import { PageHeader } from "../../features/dashboard/PageHeader";
+import { TodayFocusCard } from "../../features/dashboard/TodayFocusCard";
 
 export function ToolsRoute(){
    return(
       <>
       <Navbar />
       <PageHeader />
+      <TodayFocusCard />
       <Footer />
+      
       </>
-    
    )
 }
 
