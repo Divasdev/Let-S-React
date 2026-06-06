9165142cee0a4b7bdda9a2514082451af2a06d58
Divasdev <sharmadivas881@gmail.com>
2026-06-06 11:24:08 +0530

    feat(about): style and clean up about page container

--- Daily-flow/src/pages/About/about.jsx
+++ Daily-flow/src/pages/About/about.jsx
@@ -1,12 +1,22 @@
 import { ProductShowcase } from "./ProductShowcase";
 
 import { CoreValueGrid } from "./CoreValuesGrid";
+
+import { MissionCard } from "./MissionCard";
+import { MissionHero } from "./MissionHero";
+import { Navbar} from "../../components/layout/Navbar";
+
+import { Footer} from "../../components/layout/Footer"
 import "./about.css";
 export function About(){
    return(
       <>
+         <Navbar />
+         <MissionHero />
+         <MissionCard />
          <CoreValueGrid />
          <ProductShowcase />
+         <Footer />
       </>
    );
 }
