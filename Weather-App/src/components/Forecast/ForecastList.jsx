2d9b03506a09ec97ab0ebf72c54810c29fff51c5
Divasdev <sharmadivas881@gmail.com>
2026-06-11 16:36:58 +0530

    fix(forecast): update ForecastList to pass individual props to ForecastRow

--- Weather-App/src/components/Forecast/ForecastList.jsx
+++ Weather-App/src/components/Forecast/ForecastList.jsx
@@ -1,25 +1,25 @@
 import { ForecastRow } from "./ForecastRow";
 import { TabGroup } from "./Forecast-Header/TabGroup";
-
+import { weatherData } from "../../../data.js";
 import "./ForecastList.css";
 
 export function ForecastList(){
    return(
       <div className="forecast-container">
         <TabGroup />
-
         <div className="forecast-rows">
-          <ForecastRow />
-          <ForecastRow />
-          <ForecastRow />
-          <ForecastRow />
-          <ForecastRow />
-          <ForecastRow />
-          <ForecastRow />
-          <ForecastRow />
+          {weatherData.hourlyForecast.map((item) => (
+            <ForecastRow
+              key={item.id}
+              time={item.time}
+              icon={item.icon}
+              condition={item.condition}
+              temp={item.temperature}
+              wind={null}
+              humidity={null}
+            />
+          ))}
         </div>
-
-        <div className="scroll-indicator">▼</div>
       </div>
    )
 }
