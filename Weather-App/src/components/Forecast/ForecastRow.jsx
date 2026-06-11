83779cf4f0244914156b21be3898081c53688ec2
Divasdev <sharmadivas881@gmail.com>
2026-06-11 16:36:52 +0530

    fix(forecast): refactor ForecastRow to individual props and fix JSX nesting

--- Weather-App/src/components/Forecast/ForecastRow.jsx
+++ Weather-App/src/components/Forecast/ForecastRow.jsx
@@ -1,34 +1,32 @@
 import "./ForecastRow.css";
 
-export function ForecastRow(){
-   return(
-        <div className="forecast-card">
-          <div className="forecast-row">
+export function ForecastRow({ time, icon, condition, temp, wind, humidity }) {
+  const tempC = (((temp ?? 32) - 32) * 5 / 9).toFixed(1);
 
-            <div className="forecast-left">
-              <div className="forecast-weather-icon">⛈️</div>
+  return (
+    <div className="forecast-card">
+      <div className="forecast-row">
 
-              <div>
-                <p className="forecast-time">1AM</p>
-                <h3 className="forecast-condition">Mostly Cloudy</h3>
-              </div>
-            </div>
-
-            <div className="forecast-divider"></div>
-
-            <div className="forecast-temp">
-              12<span>°C</span>
-            </div>
-
-            <div className="forecast-extra">
-              <p>Wind: 120km</p>
-              <p>Humidity: 59%</p>
-            </div>
-
+        <div className="forecast-left">
+          <div className="forecast-weather-icon">⛈️</div>
+          <div>
+            <p className="forecast-time">{time}</p>
+            <h3 className="forecast-condition">{condition}</h3>
           </div>
         </div>
-         
 
-   )
+        <div className="forecast-divider"></div>
+
+        <div className="forecast-temp">
+          {tempC}<span>°C</span>
+        </div>
+
+        <div className="forecast-extra">
+          <p>{wind ?? "Wind: N/A"}</p>
+          <p>{humidity ?? "Humidity: N/A"}</p>
+        </div>
+
+      </div>
+    </div>
+  );
 }
-
