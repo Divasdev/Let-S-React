32158c09109a736cc7944f04e5e321ee9fc9e67d
Divasdev <sharmadivas881@gmail.com>
2026-06-18 08:16:00 +0530

    fix(App): correct class names for layout and styling adjustments

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -10,13 +10,13 @@ function App() {
   return (
     <>
       <section id="center">
-        <div class="flex justify-end ">
+        <div class="flexjustify-between">
           
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
-        <div>
+        <div class="flex  justify-center bg-green-300">
           <h1 class="bg-amber-900">Get started</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
@@ -39,7 +39,7 @@ function App() {
             <use href="/icons.svg#documentation-icon"></use>
           </svg>
           <h2>Documentation</h2>
-          <p>Your questions, answered</p>
+          <p class="flexjustify-between">Your questions, answered</p>
           <ul>
             <li>
               <a href="https://vite.dev/" target="_blank">
