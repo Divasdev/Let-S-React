78e4c7809e16b0881c0bc55a4526df39f8908a66
Divasdev <sharmadivas881@gmail.com>
2026-06-21 20:19:58 +0530

    feat(NavBar): add NavBar component with Tailwind CSS styling

---
+++ Aero-Radar/src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import './NavBar.css';
+
+export  function NavBar(){
+
+   return (
+      <div className='bg-blue-100 text-white p-6 backdrop:backdrop-blur-lg'>
+         AeroRadar
+      </div>
+   )
+}
+
+
+
