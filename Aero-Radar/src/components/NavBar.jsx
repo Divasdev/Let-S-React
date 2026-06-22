04e2a1ceb22fa819aada2b7c2c5ca7b495dde164
Divasdev <sharmadivas881@gmail.com>
2026-06-22 11:09:47 +0530

    complete making the navbar

--- Aero-Radar/src/components/NavBar.jsx
+++ Aero-Radar/src/components/NavBar.jsx
@@ -1,13 +1,41 @@
 import './NavBar.css';
-
+import logoImg from '../assets/logo.png';
 export  function NavBar(){
 
    return (
-      <div className='bg-blue-100 text-white p-6 backdrop:backdrop-blur-lg'>
-         AeroRadar
-      </div>
+   <nav className="max-w-6xl mx-auto mt-6 px-6 ">
+   <div className="flex items-center justify-between px-8 py-4 bg-white border border-gray-200 rounded-full shadow-sm">
+      <Logo />
+      <NavLinks />
+      <CTAButton />
+   </div>
+</nav>
+    
    )
 }
 
-
-
+export function Logo(){
+   return(
+      <div className='flex items-center gap-2'>
+         <img src={logoImg} className='size-8 rounded-full' />
+         <h2 className='font-semibold text-gray-900'>AeroRadar</h2>
+      </div>
+   )
+}
+export function NavLinks(){
+   return(
+      <div className='flex items-center gap-8 text-sm font-medium text-gray-600'>
+         <a href='#'>Features</a>
+         <a href='#'>Airport</a>
+         <a href='#'>Flights</a>
+         <a href='#'>Analytics</a>
+      </div>
+   )
+}
+export function CTAButton(){
+   return (
+      <button className='bg-blue-600 text-white px-5 py-2 rounded-full text-sm font-medium hover:bg-blue-700 transition'>
+         Launch app
+      </button>
+   )
+}
