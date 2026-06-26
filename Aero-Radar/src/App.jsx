4881365b2b7b446e9950cef99ed7db3e391cc0b1
Divasdev <sharmadivas881@gmail.com>
2026-06-26 21:24:23 +0530

    Integrate SeamlessImports component into App layout

--- Aero-Radar/src/App.jsx
+++ Aero-Radar/src/App.jsx
@@ -1,10 +1,12 @@
 import { Footer } from './components/Footer/Footer';
 import { NavBar } from './components/NavBar';
 import { FAQ } from './components/FAQ/FAQ';
+import { SeamlessImports } from './components/Imports/SeamlessImports';
 function App() {
   return (
     <>
       <NavBar />
+      <SeamlessImports />
       <FAQ />
       <Footer />
     </>
