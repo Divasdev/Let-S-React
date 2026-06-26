1167cd0e17167fe77286956e8d4eeae71e435426
Divasdev <sharmadivas881@gmail.com>
2026-06-26 08:41:51 +0530

    Refactor Footer component for improved layout and styling; enhance navigation structure

--- Aero-Radar/src/components/FAQ/FAQitem.jsx
+++ Aero-Radar/src/components/FAQ/FAQitem.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 
-const faqs=[
+const faqs = [
    {
-      question:"Where do you get your flight data?",
-      answer:"We aggregate data directly from global ATC systems, FAA feeds,and airline proprietary APIs to ensure the lowest latency possible."
+      question: "Where do you get your flight data?",
+      answer: "We aggregate data directly from global ATC systems, FAA feeds,and airline proprietary APIs to ensure the lowest latency possible."
    },
    {
-      question:"How accurate is the delay prediction?",
-      answer:"Our ML models predict delays based on inbound aircraft status,historical weather patterns, and airport congestion, and airport congetion, boasting a 94% accuracy rate up to 24 hours prior to departure."
+      question: "How accurate is the delay prediction?",
+      answer: "Our ML models predict delays based on inbound aircraft status,historical weather patterns, and airport congestion, and airport congetion, boasting a 94% accuracy rate up to 24 hours prior to departure."
 
    }
 ]
@@ -15,41 +15,41 @@ const faqs=[
 
 export function FAQitem() {
    const [openIndex, setOpenIndex] = useState(null);
-  return (
-    <div className="max-w-4xl mx-auto space-y-6">
+   return (
+      <div className="max-w-4xl mx-auto space-y-6">
 
 
-       {faqs.map((faq, index) => (
+         {faqs.map((faq, index) => (
 
-          <div
-          key={faq.question} 
-          className="border border-gray-100 rounded-2xl shadow-sm px-4 hover:border-blue-300 transition-colors">
-        <button 
-        className="flex items-center justify-between w-full py-5"
-        onClick={() => {
-          setOpenIndex(openIndex === index ? null : index);
-        }}
-        >
-          <span className="text-left font-bold text-gray-900">
-           {faq.question}
-          </span>
+            <div
+               key={faq.question}
+               className="border border-gray-100 rounded-2xl shadow-sm px-4 hover:border-blue-300 transition-colors">
+               <button
+                  className="flex items-center justify-between w-full py-5"
+                  onClick={() => {
+                     setOpenIndex(openIndex === index ? null : index);
+                  }}
+               >
+                  <span className="text-left font-bold text-gray-900">
+                     {faq.question}
+                  </span>
 
-          <span 
-          className={`text-xl text-gray-500 transition-transform duration-500 ${openIndex === index ? 'rotate-180' : ''}`}
-          
-          >▼</span>
-        </button>
+                  <span
+                     className={`text-xl text-gray-500 transition-transform duration-500 ${openIndex === index ? 'rotate-180' : ''}`}
 
-        {openIndex === index && <div className="pb-6">
-          <p className="text-gray-600 leading-7">
-            {faq.answer}
-          </p>
-        </div>}
+                  >▼</span>
+               </button>
 
-      </div>
+               {openIndex === index && <div className="pb-6">
+                  <p className="text-gray-600 leading-7">
+                     {faq.answer}
+                  </p>
+               </div>}
+
+            </div>
 
          ))}
-  
-    </div>
-  );
+
+      </div>
+   );
 }
--- Aero-Radar/src/components/Footer/Footer.jsx
+++ Aero-Radar/src/components/Footer/Footer.jsx
@@ -1,43 +1,57 @@
 import logoImg from '../../assets/logo.png';
-export function Footer(){
-   return(
-      <>
-         <div>
-               <div className='flex items-center gap-2'>
-                     <img src={logoImg} className='size-8 rounded-full' />
-                     <h2 className='font-semibold text-charcoal'>AeroRadar</h2>
-               </div>
+export function Footer() {
+   return (
+      <footer className='flex justify-between max-w-7xl  mx-auto px-6 py-12'>
+         <div className='flex flex-col gap-3'>
+            <div className='flex items-center gap-2'>
+               <img src={logoImg} className='size-8 rounded-full' />
+               <h2 className='font-semibold text-charcoal'>AeroRadar</h2>
+            </div>
 
-               <p>© 2026 AeroRadar Aviation Systems.Precision engineered for the skies</p>
+            <p>© 2026 AeroRadar Aviation Systems.Precision engineered for the skies</p>
 
-               <p>Build v1.0.0 BETA RELEASE</p>
+            <p>Build v1.0.0 BETA RELEASE</p>
 
          </div>
 
-         <div>
-            <a href="#">
-               Product
-            </a>
-            <a href="#">
-               Airport
-            </a>
-            
-            <a href="#">
-               Flights
-            </a>
-            <a href="#">
-               Analytics
-            </a>
-            <a href="#">
-               Privacy 
-            </a>
-            <a href="#">
-                Contact
-            </a>
-            
-            
-         </div>
-      
-      </>
+         <nav className='grid grid-cols-3 gap-12 '>
+
+            <div className='flex flex-col gap-5 '>
+               <a href="#" className='text-sm text-outline hover:text-blue-300 transition'>
+                  Product
+               </a>
+               <a href="#" className='text-sm text-outline hover:text-blue-300 transition'>
+                  Airport
+               </a>
+
+
+            </div>
+
+
+            <div className='flex flex-col gap-5'>
+
+               <a href="#" className='text-sm text-outline hover:text-blue-300 transition'>
+                  Flights
+               </a>
+               <a href="#" className='text-sm text-outline hover:text-blue-300 transition'>
+                  Analytics
+               </a>
+            </div>
+
+            <div className='flex flex-col gap-5'>
+
+               <a href="#" className='text-sm text-outline hover:text-blue-300 transition'>
+                  Privacy
+               </a>
+               <a href="#" className='text-sm text-outline hover:text-blue-300 transition'>
+                  Contact
+               </a>
+            </div>
+
+
+
+         </nav>
+
+      </footer>
    )
 }
