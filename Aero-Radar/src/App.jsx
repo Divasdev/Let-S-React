4bfe2c8b5ac3f9cbcf396d627518835579e8150c
Divasdev <sharmadivas881@gmail.com>
2026-06-28 21:38:06 +0530

    Integrate Airport Intelligence section into App layout

--- Aero-Radar/src/App.jsx
+++ Aero-Radar/src/App.jsx
@@ -2,11 +2,13 @@ import { Footer } from './components/Footer/Footer';
 import { NavBar } from './components/NavBar';
 import { FAQ } from './components/FAQ/FAQ';
 import { SeamlessImports } from './components/Imports/SeamlessImports';
+import { Airport } from './components/AirportIntelligenceSection/Airport';
 function App() {
   return (
     <>
       <NavBar />
       <SeamlessImports />
+      <Airport />
       <FAQ />
       <Footer />
     </>
