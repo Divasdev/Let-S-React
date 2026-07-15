2bf619d79af2f6b9fb98dbc510ab9fc4cb899397
Divasdev <sharmadivas881@gmail.com>
2026-07-16 00:16:49 +0530

    feat(TowerCard): render signalImg icon inside container

--- Aero-Radar/src/components/FeaturesSection/TowerCard.jsx
+++ Aero-Radar/src/components/FeaturesSection/TowerCard.jsx
@@ -3,6 +3,9 @@ import signalImg from "../../assets/signal.png";
 export function TowerCard() {
    return (
       <div className=" max-w-sm  shadow-lg bg-amber-50 p-10 mb-15">
+         <img src={signalImg}
+            className="w-10 h-10 object-contain mb-5"
+         />
       </div>
    )
 }
