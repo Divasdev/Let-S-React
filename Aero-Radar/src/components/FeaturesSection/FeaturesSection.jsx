9b3cf84c89ed55453ff8a31db9fd4f12213f7928
Divasdev <sharmadivas881@gmail.com>
2026-07-15 23:30:24 +0530

    feat: implement basic structure for FeaturesSection component

---
+++ Aero-Radar/src/components/FeaturesSection/FeaturesSection.jsx
@@ -0,0 +1,12 @@
+export function FeaturesSection(){
+   return (
+      <section className="px-8 md:py-16 py-16">
+       <h1 className="font-bold text-3xl tracking-tight text-gray-900 ">
+         Everything you've ever wanted to know.
+      </h1>
+      <p className=" max-w-xl mt-4 text-gray-600 ">
+         AeroRadar keeps you informed by tracking more information about your flight than any other app.
+      </p>
+      </section>
+   )
+}
