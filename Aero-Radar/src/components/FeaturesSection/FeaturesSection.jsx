36ca751b28f2c68e29fbf17df12f678e5f7cc13d
Divasdev <sharmadivas881@gmail.com>
2026-07-16 00:17:27 +0530

    feat(FeaturesSection): import and render TowerCard component

--- Aero-Radar/src/components/FeaturesSection/FeaturesSection.jsx
+++ Aero-Radar/src/components/FeaturesSection/FeaturesSection.jsx
@@ -1,12 +1,20 @@
+import { TowerCard } from "./TowerCard"
 export function FeaturesSection(){
    return (
+      <>
       <section className="px-8 md:py-16 py-16">
        <h1 className="font-bold text-3xl tracking-tight text-gray-900 ">
-         Everything you've ever wanted to know.
-      </h1>
-      <p className=" max-w-xl mt-4 text-gray-600 ">
-         AeroRadar keeps you informed by tracking more information about your flight than any other app.
-      </p>
+          Everything you've ever wanted to know.
+       </h1>
+       <p className=" max-w-xl mt-4 text-gray-600 ">
+          AeroRadar keeps you informed by tracking more information about your flight than any other app.
+       </p>
+
       </section>
+      <TowerCard />
+      
+      </>
+
+      
    )
 }
