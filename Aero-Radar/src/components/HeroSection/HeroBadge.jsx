384b56b356dc51d793a6ff83286869752f5b1a3f
Divasdev <sharmadivas881@gmail.com>
2026-07-17 15:28:11 +0530

    feat(hero): update HeroBadge styles

--- Aero-Radar/src/components/HeroSection/HeroBadge.jsx
+++ Aero-Radar/src/components/HeroSection/HeroBadge.jsx
@@ -1,8 +1,10 @@
-export function HeroBadge(){
+export function HeroBadge() {
    return (
-      <span class="bg-neutral-primary-soft border border-default text-heading text-xs font-medium px-1.5 py-0.5 rounded-full">
-         SYSTEMS OPERATIONAL
-       </span>
-
+      <span className="inline-flex items-center gap-2 px-4 py-1.5 rounded-full
+         bg-gray-500/10 text-xs font-mono font-bold
+         uppercase tracking-widest  text-center">
+         <span className="size-2 rounded-full bg-green-500 animate-pulse  flex justify-center" />
+          SYSTEMS OPERATIONAL
+      </span>
    )
 }
