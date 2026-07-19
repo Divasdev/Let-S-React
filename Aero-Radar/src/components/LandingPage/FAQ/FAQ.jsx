a2122b54812aaf3e15533b35511547697cad9042
Divasdev <sharmadivas881@gmail.com>
2026-07-19 21:21:49 +0530

    refactor: update structure for FAQ.jsx

---
+++ Aero-Radar/src/components/LandingPage/FAQ/FAQ.jsx
@@ -0,0 +1,14 @@
+import { FAQitem } from "./FAQitem"
+
+export function FAQ() {
+   return (
+      <section className="py-20 px-6">
+         <h2 className="text-center text-charcoal font-bold text-3xl tracking-tight mb-10">
+            Frequently Asked Questions
+         
+         </h2>
+
+         <FAQitem />
+      </section>
+   )
+}
