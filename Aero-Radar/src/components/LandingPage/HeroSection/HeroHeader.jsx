69092004fd3b520d162798e758db039f922a0b4b
Divasdev <sharmadivas881@gmail.com>
2026-07-19 21:21:49 +0530

    refactor: update structure for HeroHeader.jsx

---
+++ Aero-Radar/src/components/LandingPage/HeroSection/HeroHeader.jsx
@@ -0,0 +1,18 @@
+export function HeroHeader() {
+   return (
+      <>
+
+         <h1 className="font-extrabold text-4xl text-center mb-5">
+            Get the truth <br />
+            when you travel.
+         </h1>
+
+         <p className="text-gray-600 text-center font-sans mb-20">
+            
+            The only app that tells you everything about your flight.
+
+         </p>
+
+      </>
+   )
+}
