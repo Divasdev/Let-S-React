24ace0de124a68093f5fd441b0a679b9cbe7b7c3
Divasdev <sharmadivas881@gmail.com>
2026-07-23 13:58:07 +0530

    feat(routing): configure React Router with BrowserRouter and Routes

--- Aero-Radar/src/App.jsx
+++ Aero-Radar/src/App.jsx
@@ -1,6 +1,16 @@
-// import { LandingPage } from './pages/LandingPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { LandingPage } from './pages/LandingPage';
 import { AirportPage } from './pages/AirportPage';
+
 function App() {
-  return <AirportPage />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/airport" element={<AirportPage />} />
+      </Routes>
+    </Router>
+  );
 }
+
 export default App;
